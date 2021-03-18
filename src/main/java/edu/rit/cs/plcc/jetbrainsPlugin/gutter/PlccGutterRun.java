709418c20f2a27ea25b233fcafd29d8d1a5868f4
Mark Nash <mdn4993@rit.edu>
2021-03-17 22:55:18 -0400

    Partial commit working on running from the gutter

---
+++ src/main/java/edu/rit/cs/plcc/jetbrainsPlugin/gutter/PlccGutterRun.java
@@ -0,0 +1,33 @@
+package edu.rit.cs.plcc.jetbrainsPlugin.gutter;
+
+import com.intellij.execution.lineMarker.RunLineMarkerContributor;
+import com.intellij.psi.PsiElement;
+import edu.rit.cs.plcc.jetbrainsPlugin.lang.parser_model.PLCCTypes;
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static edu.rit.cs.plcc.jetbrainsPlugin.util.Icons.runIcon;
+
+public class PlccGutterRun extends RunLineMarkerContributor {
+
+    @Override
+    public @Nullable Info getInfo(@NotNull PsiElement psiElement) {
+        // Only show one icon at the top of the file where the first regex definition is
+        if (psiElement.getNode().getElementType() != PLCCTypes.REGEX_DEFS) {
+            return null;
+        }
+
+        val fileName = psiElement.getContainingFile().getName();
+
+        return new Info(
+                runIcon,
+                x -> "Run ".concat(fileName).concat(" with PLCC"),
+
+        );
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/edu/rit/cs/plcc/jetbrainsPlugin/util/Icons.java
@@ -0,0 +1,9 @@
+package edu.rit.cs.plcc.jetbrainsPlugin.util;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface Icons {
+    Icon runIcon = IconLoader.getIcon("/icons/run.svg", Icons.class);
+}
