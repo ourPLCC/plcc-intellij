39ac2335d4fd52c3129ae5e0e503721d8473517e
Mark Nash <abacefabmicroblast@hotmail.com>
2021-03-18 01:25:17 -0400

    At least the run button shows up. It only is a shortcut to the run configurations though

--- src/main/java/edu/rit/cs/plcc/jetbrainsPlugin/gutter/PlccGutterRun.java
+++ src/main/java/edu/rit/cs/plcc/jetbrainsPlugin/gutter/PlccGutterRun.java
@@ -1,6 +1,7 @@
 package edu.rit.cs.plcc.jetbrainsPlugin.gutter;
 
 import com.intellij.execution.lineMarker.RunLineMarkerContributor;
+import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.psi.PsiElement;
 import edu.rit.cs.plcc.jetbrainsPlugin.lang.parser_model.PLCCTypes;
 import lombok.val;
@@ -14,20 +15,27 @@ public class PlccGutterRun extends RunLineMarkerContributor {
     @Override
     public @Nullable Info getInfo(@NotNull PsiElement psiElement) {
         // Only show one icon at the top of the file where the first regex definition is
-        if (psiElement.getNode().getElementType() != PLCCTypes.REGEX_DEFS) {
+        val elementType = psiElement.getNode().getElementType();
+        if (elementType != PLCCTypes.TOKEN_TYPE_NAME) {
+            return null;
+        }
+
+        val tokenDefsSection = psiElement.getParent().getParent().getParent();
+        val firstTokenLine = tokenDefsSection.getFirstChild();
+        val currentTokenLine = psiElement.getParent().getParent();
+        if (!currentTokenLine.equals(firstTokenLine)) {
             return null;
         }
 
         val fileName = psiElement.getContainingFile().getName();
 
+        val actionManager = ActionManager.getInstance();
+        val editConfigs = actionManager.getAction("editRunConfigurations");
+
         return new Info(
                 runIcon,
                 x -> "Run ".concat(fileName).concat(" with PLCC"),
-
+                editConfigs
         );
-    }
-
-    public static void main(String[] args) {
-
     }
 }
